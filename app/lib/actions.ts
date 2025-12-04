4ccb2e66edd710a07cfc977f3167defd146747e9
vickbk <vickbake8@gmail.com>
2025-12-04 17:43:00 +0200

    adding loggin logic

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
@@ -100,4 +102,20 @@ export async function deleteInvoice(id: string) {
     return { message: "Database Error: Failed to Delete Invoice." };
   }
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(_: unknown, formData: FormData) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
