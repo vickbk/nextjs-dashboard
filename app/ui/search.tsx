f5b24fffaf57fdfcc63dbabadb1dd9d26b2cdf8e
vickbk <vickbake8@gmail.com>
2025-12-04 11:36:58 +0200

    syncing the default search value

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,6 +8,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const params = new URLSearchParams(searchParams);
   const pathname = usePathname();
   const { replace } = useRouter();
+
   function handleSearch(term: string) {
     if (term) {
       params.set("query", term);
@@ -16,6 +17,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -25,6 +27,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={({ target: { value: term } }) => handleSearch(term)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
