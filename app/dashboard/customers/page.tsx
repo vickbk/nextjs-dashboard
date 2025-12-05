0f936527e4041a3edb7c1c581470452f5430bd56
vickbk <vickbake8@gmail.com>
2025-12-05 11:41:58 +0200

    Adding meta data to pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 export default function Page() {
-    return <p>Customer Page</p>;
-  }
+  return <p>Customer Page</p>;
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
+import { title } from "process";
 import { Suspense } from "react";
+
+export const metadata = {
+  title: "Login",
+};
 
 export default function LoginPage() {
   return (
